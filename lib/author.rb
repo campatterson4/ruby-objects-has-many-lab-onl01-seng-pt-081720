17d50b3c1670bc6a4326ddd5de4780f7a4b4ddf7
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-11 19:53:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,31 @@
-class Author
-attr_accessor :name, :posts
+class Author 
+  attr_accessor :name
+  @@post_count = 0
 
-@@post_count = 0
-  
-def initialize(name)
-@name = name
-@posts = []
-end
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-def add_post(post)
-@posts << post
-post.author = self
-@@post_count += 1
-end
+  def posts
+    @posts
+  end
 
-def add_post_by_title(title)
-post = Post.new(title)
-@posts << post
-post.author = self
-@@post_count += 1
-end
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 
-def self.post_count
-@@post_count
-end
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
 end
